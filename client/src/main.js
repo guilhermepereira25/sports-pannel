29f8c387bf9ce85d0869f21c217f2bef13484da9
dev-guilherme <guilhermepereiradv@gmail.com>
2023-09-28 23:15:47 -0300

    feat: adding bootstrap in main

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.mount('#app');
