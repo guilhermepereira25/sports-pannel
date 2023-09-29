e45e9bc961d66f593516dc5d4cb7e9e2b443b315
dev-guilherme <guilhermepereiradv@gmail.com>
2023-09-28 23:17:25 -0300

    feat: app vue adjusted to routes basic

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,31 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, computed } from 'vue'
+import Home from './components/Home.vue'
+import Login from './components/auth/Login.vue'
+
+const routes = {
+  '/': Home,
+  '/login': Login,
+  '/home': Home
+}
+
+const currentPath = ref(window.location.hash)
+
+// if (currentPath !== '#/login' && sessionStorage.getItem('token') === null) {
+//   window.location.href = '/#/login'
+// }
+
+window.addEventListener('hashchange', () => {
+  currentPath.value = window.location.hash
+})
+
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || '/'] || NotFound
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <component :is="currentView" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
